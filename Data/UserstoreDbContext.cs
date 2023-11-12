99a7a6642aa2de0435ec628bebe4eb214ce2850c
Konstantin <forever_021@mail.ru>
2023-11-12 13:23:59 +0300

    Добавьте файлы проекта.

---
+++ Data/UserstoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace InformationSecurityConference.Data
+{
+    internal class UserstoreDbContext:DbContext
+    {
+        public UserstoreDbContext():base("name=ConnectionString")
+        {
+                
+        }
+    }
+}
